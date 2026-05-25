0dfe462760572bc6ac4dd01ae9f81471d5fba95e
Henri Bourcereau <henri.bourcereau@gmail.com>
2026-05-25 21:43:31 +0200

    fix: flake frontendCargoDeps hash

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
               frontendCargoDeps = rustPlatform.fetchCargoVendor {
                 src = ./.;
                 name = "trictrac-frontend-vendor";
-                hash = "sha256-E3MJbEehbXni7B8fQPS8fSri4f2b0A33r2djiK81E2Y=";
+                hash = "sha256-R75zHXKhv0nkBv6U24ZCAtwp1yaSbr0bFywWQHwhSkM=";
               };
             in
             final.stdenv.mkDerivation {

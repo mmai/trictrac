cd1e7802f7cb86e4424fa5f2dc10178b3e002984
Henri Bourcereau <henri.bourcereau@gmail.com>
2026-05-25 21:44:00 +0200

    chore: bump version to 0.2.15

--- flake.nix
+++ flake.nix
@@ -103,7 +103,7 @@
 
           trictrac = with final; rustPlatform.buildRustPackage {
             pname = "trictrac";
-            version = "0.2.14"; # trictrac-version
+            version = "0.2.15"; # trictrac-version
             src = ./.;
 
             nativeBuildInputs = [ pkg-config ];

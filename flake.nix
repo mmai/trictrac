1cdb1380b4f0f4932087fd9d34b6850a6624ff51
Henri Bourcereau <henri.bourcereau@gmail.com>
2026-06-21 13:46:48 +0200

    chore: bump version to 0.2.18

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
               frontendCargoDeps = rustPlatform.fetchCargoVendor {
                 src = ./.;
                 name = "trictrac-frontend-vendor";
-                hash = "sha256-XBxdRT/f69GDfVc18/DnnAiY1vjMGMWfcYot0K0jevg=";
+                hash = "sha256-HJ9iUzsaMKapKb9DH0aoX7keuShq5fnHk1TSvNlw2CQ=";
               };
               # Must match the wasm-bindgen version in Cargo.lock
               wasm-bindgen-version = "0.2.118";
@@ -102,7 +102,7 @@
 
           trictrac = with final; rustPlatform.buildRustPackage {
             pname = "trictrac";
-            version = "0.2.16"; # trictrac-version
+            version = "0.2.18"; # trictrac-version
             src = ./.;
 
             nativeBuildInputs = [ pkg-config ];
